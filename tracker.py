e4ff7ad144dc6679eb69d0d7e771c26ca180a747
sanpadGH <david@sanchezpadilla.com>
2026-03-04 17:08:23 +0100

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -3,6 +3,7 @@ from sources.ctgov import fetch_phase3_recent
 from sources.ema_chmp_under_eval import fetch_ema_under_review_chmp
 from sources.ctis import fetch_ctis_phase3
 from sources.fda import fetch_fda_under_review
+from sources.ema_approvals import fetch_ema_approvals
 from sinks.sheets import upsert_events
 
 def main():
@@ -24,8 +25,10 @@ def main():
 
     fda_events = fetch_fda_under_review()
 
-    all_events = ema_events + fda_events + ctis_events + ctgov_events
-
+    ema_approval_events = fetch_ema_approvals()
+    
+    all_events = ema_events + ema_approval_events + fda_events + ctis_events + ctgov_events
+    
     inserted = upsert_events(spreadsheet_id, worksheet, all_events)
     print("Inserted rows:", inserted)
 
